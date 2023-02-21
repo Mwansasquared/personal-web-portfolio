3b12b55c13501930d20371632134bfa772b487ce
Mwansasquared <mwansasquared9@gmail.com>
2023-02-21 15:38:12 +0200

    Added CSS for about-me section

--- src/Components/about-me.js
+++ src/Components/about-me.js
@@ -1,11 +1,11 @@
 import React from "react";
 import '../styling/about-me.css';
-// import images from '../images/images.jpeg';
+import images from '../images/images.jpeg';
 
 class AboutMe extends React.Component {
     render () {
         return (
-            <div className="about-me">
+            <div className="about-me-component">
                 <div>
                     {/* <img id="background-image" src={image} alt="Me"></img> */}
                     <p id="about-me">
@@ -15,10 +15,13 @@ class AboutMe extends React.Component {
                 
                 <div className="about-me-section">
                     <h2>Here's little bit about me</h2>
-                    <p className="about-me-description">
-                        I'm a software developer with a Bachelor's degree in Computer Engineering from the Copperbelt University.<br />
-                        I love writing and reading, and all things tech.   
-                    </p>
+                    <div className="about-me-div">
+                        <p className="about-me-description">
+                            I'm a software developer with a Bachelor's degree in Computer Engineering from the Copperbelt University.<br />
+                            I love writing and reading, and all things tech.   
+                        </p>
+                        <img src={images} alt=""></img>
+                    </div>
                 </div>
             </div>
         )
--- src/Components/blogs.js
+++ src/Components/blogs.js
@@ -6,9 +6,28 @@ class Blogs extends React.Component {
         return (
             <div id="blogs">
                 <h2>Blogs</h2>
-                <p>Here a few articles I have written on various topics, and some books I have authored. <br />
-                Like what you see and need some written work to be done? <a href="#contact">Leave me a message</a> </p>
+                <div>
+                    <p>Here a few articles I have written on various topics, and some books I have authored. <br />
+                    Like what you see and need some written work to be done? <a href="#contact">Leave me a message</a>
+                    </p>
+                </div>
+                
+                <div className="projects-section">
+                    <section className="projects">
+                        <h3>RFID Driving License</h3>
+                        <p>RFID driving licence web app built with javascript.</p>
+                    </section>
+                    <section className="projects">
+                        <h3>Palindrome Checker</h3>
+                        <p>Simple palindrome checker built with plain javascript</p>
+                    </section>
+                    <section className="projects">
+                        <h3>Portfolio</h3>
+                        <p>Simple portfolio website built using ReactJS</p>
+                    </section>
+                </div>
             </div>
+            
         )
     }
 }
