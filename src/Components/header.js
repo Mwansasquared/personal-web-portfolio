1559c4bbfe841f74064e56e3e617459853710a2e
Mwansasquared <mwansasquared9@gmail.com>
2023-03-02 10:13:06 +0200

    Responsive navbar header

--- src/Components/header.js
+++ src/Components/header.js
@@ -3,10 +3,10 @@ import React from "react";
 class Header extends React.Component {
     render () {
         return (
-            <header className="heade bg-slate-300 border-gray-200 h-16 px-80 py-2">
+            <header className="header flex-auto bg-slate-300 border-gray-200 h-16 py-2">
                 <div>
                         <nav className="top-nv">
-                            <ul className="flex justify-between">
+                            <ul className="flex justify-around">
                                 <li className="top-nav-list mr-3">
                                     <a href="#aboutMe">About Me</a>
                                 </li>
